7bec7180ef7fb53e6d9e3aec0c87f73d93391c18
CherifiMi <cherifi154949@gmail.com>
2022-03-31 00:47:25 +0100

    added back to my location btn

--- app/src/main/java/com/example/locationpicker/MainActivity.kt
+++ app/src/main/java/com/example/locationpicker/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.locationpicker
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import com.example.locationpicker.databinding.ActivityMainBinding
 import com.mapbox.geojson.Point
@@ -15,6 +19,7 @@ import com.mapbox.maps.Style
 import com.mapbox.maps.plugin.animation.MapAnimationOptions
 import com.mapbox.maps.plugin.animation.camera
 import com.mapbox.maps.plugin.locationcomponent.location
+import com.mapbox.maps.plugin.overlay.mapboxOverlay
 import com.mapbox.navigation.base.options.NavigationOptions
 import com.mapbox.navigation.core.MapboxNavigation
 import com.mapbox.navigation.core.trip.session.LocationMatcherResult
@@ -26,8 +31,10 @@ class MainActivity : AppCompatActivity() {
 
     //-----------------values
     private val navigationLocationProvider = NavigationLocationProvider()
+
     private val locationObserver = object : LocationObserver {
         override fun onNewRawLocation(rawLocation: Location) {
+
         }
         override fun onNewLocationMatcherResult(locationMatcherResult: LocationMatcherResult) {
             val enhancedLocation = locationMatcherResult.enhancedLocation
@@ -35,13 +42,23 @@ class MainActivity : AppCompatActivity() {
                 enhancedLocation,
                 locationMatcherResult.keyPoints,
             )
-            updateCamera(enhancedLocation)
+
+            if (moves){
+                updateCamera(enhancedLocation)
+                moves = false
+            }
+
+
         }
     }
     private lateinit var mapboxMap: MapboxMap
     private lateinit var mapboxNavigation: MapboxNavigation
+
     private val FINE_LOCATION_RQ =101
+
     private lateinit var binding: ActivityMainBinding
+
+    var moves = true
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,12 +73,16 @@ class MainActivity : AppCompatActivity() {
         binding.mapView.location.apply {
             setLocationProvider(navigationLocationProvider)
         }
+        binding.backBtn.setOnClickListener{
+            moves = true
+        }
+
         init()
     }
 
     private fun checkPermission(permission: String, name: String, requestCode: Int) {
         if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, "$name permission is granted", Toast.LENGTH_LONG).show()
+            //Toast.makeText(this, "$name permission is granted", Toast.LENGTH_LONG).show()
         }else{
             requestPermissions(arrayOf(permission), requestCode)
         }
@@ -92,15 +113,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateCamera(location: Location) {
+
         val mapAnimationOptions = MapAnimationOptions.Builder().duration(1500L).build()
-        binding.mapView.camera.easeTo(
+        binding.mapView.camera.flyTo(
             CameraOptions.Builder()
                 .center(Point.fromLngLat(location.longitude, location.latitude))
-                //.zoom(12.0)
-                .padding(EdgeInsets(500.0, 0.0, 0.0, 0.0))
+                .zoom(16.0)
                 .build(),
             mapAnimationOptions
         )
+
+        //binding.mapView.getMapboxMap().cameraState.center.latitude()
+
+
+
     }
 
     override fun onDestroy() {
