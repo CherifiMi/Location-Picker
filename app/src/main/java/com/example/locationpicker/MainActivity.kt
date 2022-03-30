f4e31bcacc8f36a6845997af90777bbdf43d6430
CherifiMi <cherifi154949@gmail.com>
2022-03-30 22:53:36 +0100

    added current location renderer

--- app/src/main/java/com/example/locationpicker/MainActivity.kt
+++ app/src/main/java/com/example/locationpicker/MainActivity.kt
@@ -1,11 +1,99 @@
 package com.example.locationpicker
 
+import android.annotation.SuppressLint
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.locationpicker.databinding.ActivityMainBinding
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.EdgeInsets
+import com.mapbox.maps.MapboxMap
+import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.animation.MapAnimationOptions
+import com.mapbox.maps.plugin.animation.camera
+import com.mapbox.maps.plugin.locationcomponent.location
+import com.mapbox.navigation.base.options.NavigationOptions
+import com.mapbox.navigation.core.MapboxNavigation
+import com.mapbox.navigation.core.trip.session.LocationMatcherResult
+import com.mapbox.navigation.core.trip.session.LocationObserver
+import com.mapbox.navigation.ui.maps.location.NavigationLocationProvider
+
 
 class MainActivity : AppCompatActivity() {
+
+    //-----------------values
+    private val navigationLocationProvider = NavigationLocationProvider()
+    private val locationObserver = object : LocationObserver {
+        override fun onNewRawLocation(rawLocation: Location) {
+        }
+        override fun onNewLocationMatcherResult(locationMatcherResult: LocationMatcherResult) {
+            val enhancedLocation = locationMatcherResult.enhancedLocation
+            navigationLocationProvider.changePosition(
+                enhancedLocation,
+                locationMatcherResult.keyPoints,
+            )
+            updateCamera(enhancedLocation)
+        }
+    }
+    private lateinit var mapboxMap: MapboxMap
+    private lateinit var mapboxNavigation: MapboxNavigation
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        mapboxMap = binding.mapView.getMapboxMap()
+        binding.mapView.location.apply {
+            setLocationProvider(navigationLocationProvider)
+        }
+        init()
     }
+
+    private fun init() {
+        initStyle()
+        initNavigation()
+    }
+
+    private fun initStyle() {
+        mapboxMap.loadStyleUri(Style.MAPBOX_STREETS)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun initNavigation() {
+        mapboxNavigation = MapboxNavigation(
+            NavigationOptions.Builder(this)
+                .accessToken(getString(R.string.mapbox_access_token))
+                .build()
+        ).apply {
+
+            startTripSession()
+
+            registerLocationObserver(locationObserver)
+        }
+    }
+
+    private fun updateCamera(location: Location) {
+        val mapAnimationOptions = MapAnimationOptions.Builder().duration(1500L).build()
+        binding.mapView.camera.easeTo(
+            CameraOptions.Builder()
+                .center(Point.fromLngLat(location.longitude, location.latitude))
+                .zoom(12.0)
+                .padding(EdgeInsets(500.0, 0.0, 0.0, 0.0))
+                .build(),
+            mapAnimationOptions
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapboxNavigation.stopTripSession()
+        mapboxNavigation.unregisterLocationObserver(locationObserver)
+    }
+
 }
